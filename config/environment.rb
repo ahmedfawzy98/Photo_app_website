ac7f52e8633b2776996db73c251edc4baa2a40d1
ahmed <ahmedfawzy671998@gmailcom>
2019-07-22 03:26:48 +0200

    deploy issues#6

--- config/environment.rb
+++ config/environment.rb
@@ -2,14 +2,14 @@
 require_relative 'application'
 
 app_environment_variables = File.join(Rails.root, 'config', 'app_environment_variables.rb')
-load(app_environment_variables) if File.exists?(app_environment_variables)
+load(app_environment_variables) if File.exist?(app_environment_variables)
 # Initialize the Rails application.
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
+    :domain => 'photo-app-ahmed.herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
