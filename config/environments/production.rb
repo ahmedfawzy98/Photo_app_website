c2c2e8fd6ca0eb84fc7abe75b74dbbc18bc8aaa4
ahmed <ahmedfawzy671998@gmailcom>
2019-07-22 17:45:51 +0200

    deploy issues#19

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,18 +12,18 @@ Rails.application.configure do
 
   # config.action_mailer.raise_delivery_errors = false
   # config.action_mailer.default_url_options = { host: 'smtp.sendgrid.net'}
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'photo-app-ahmed.herokuapp.com' }
-  # config.action_mailer.smtp_settings = {
-  #     :user_name => ENV['SENDGRID_USERNAME'],
-  #     :password => ENV['SENDGRID_PASSWORD'],
-  #     :address => 'smtp.sendgrid.net',
-  #     :domain => 'photo-app-ahmed.herokuapp.com',
-  #     :port => 587,
-  #     :authentication => :plain,
-  #     :enable_starttls_auto => true
-  # }
+  config.action_mailer.smtp_settings = {
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :address => 'smtp.sendgrid.net',
+      :domain => 'photo-app-ahmed.herokuapp.com',
+      :port => 587,
+      :authentication => :plain,
+      :enable_starttls_auto => true
+  }
 
 
   # config.action_mailer.delivery_method = :smtp
