3dc29dc9102b21292f2849aec24d3c3746ee8c4a
ahmed <ahmedfawzy671998@gmailcom>
2019-07-25 01:17:23 +0200

    add payment infromation to sign_up users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_23_213607) do
+ActiveRecord::Schema.define(version: 2019_07_24_010007) do
 
   create_table "payments", force: :cascade do |t|
     t.string "email"
